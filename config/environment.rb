08745c57c09175d7da21c80bb212243de3bcdc6b
Alex Merecka <merecka@gmail.com>
2019-04-18 19:55:10 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,12 @@
 require 'bundler'
 Bundler.require
 
-module Concerns
-end
+# module Concerns
+# end
 
 require_all 'lib'
+
+require_relative '../lib/concerns/concerns.rb'
+require_relative '../lib/concerns/artist.rb'
+require_relative '../lib/concerns/genre.rb'
+require_relative '../lib/concerns/song.rb'
