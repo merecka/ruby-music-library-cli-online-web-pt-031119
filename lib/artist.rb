2f3378fa867e59ff61835385f8f06e7f2b5a6a72
Alex Merecka <merecka@gmail.com>
2019-04-18 03:56:58 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Artist
 
   attr_accessor :name, :songs
@@ -30,6 +32,16 @@ class Artist
     new_artist = self.new(artist)
     new_artist.save
     new_artist
+  end
+
+  def genres
+  #  binding.pry
+    genres_arry = []
+    self.songs.collect do |x|
+      genres_arry << x.genre
+    end
+    binding.pry
+    genres_arry.uniq!
   end
 
 end
