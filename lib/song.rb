6f8fb0b9b17c9d03474227704e95dfd49d6b117d
Alex Merecka <merecka@gmail.com>
2019-04-18 03:18:03 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,37 @@
 class Song
 
-  attr_accessor :name, :artist, :genre
+  attr_accessor :name, :genre
 
   @@all = []
 
-  def initialize(name, artist=nil)
+  def artist
+    @artist
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre
+    @genre
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs.include?(self) ? nil : genre.songs << self
+  end
+
+  def initialize(name, artist=nil, genre=nil)
     @name = name
     @artist = artist
+    @genre = genre
+    if genre != nil
+      self.genre=(genre)
+    end
+    if artist != nil
+      self.artist=(artist)
+    end
   end
 
   def self.all
